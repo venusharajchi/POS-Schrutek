a2ca9f12dfcff19d87b0050f9b631e975fbcb86d
Venus <har210710@spengergasse.at>
2023-06-25 19:04:20 +0200

    add Dtos

--- SPG.Venus.Tierheim.Domain/Dtos/NewTierheimDto.cs
+++ SPG.Venus.Tierheim.Domain/Dtos/NewTierheimDto.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace SPG.Venus.Tierheim.Domain.Dtos
 {
-	public class CreateTierheimDto
-	{
-		public CreateTierheimDto()
-		{
-		}
-	}
+    public class NewTierheimDto
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string Street { get; set; } = string.Empty;
+        public string Number { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
 }
 
