8e872c4dec68c3801da9b83828707d3650945f62
Venus <har210710@spengergasse.at>
2023-06-29 20:54:34 +0200

    fix razor pages

--- SPG.Venus.Tierheim.RazorPages/Pages/Tierheim/Index.cshtml.cs
+++ SPG.Venus.Tierheim.RazorPages/Pages/Tierheim/Index.cshtml.cs
@@ -19,33 +19,6 @@ namespace SPG.Venus.Tierheim.RazorPages.Pages.Tierheim
         }
 
         public List<Tierheimhaus> Tierheims { get; set; }
-
-        /*
-        public void OnGet()
-        {
-            Tierheims = new List<Tierheimhaus>
-            {
-                new Tierheimhaus(Guid.NewGuid(),
-                    "Test Tierheim 1",
-                    new Addresse("Musterstraße 1", "123", "Musterstadt 1", "Musterland 1"),
-                    TimeSpan.FromHours(8),
-                    TimeSpan.FromHours(17)),
-
-                new Tierheimhaus(Guid.NewGuid(),
-                    "Test Tierheim 2",
-                    new Addresse("Musterstraße 2", "456", "Musterstadt 2", "Musterland 2"),
-                    TimeSpan.FromHours(9),
-                    TimeSpan.FromHours(18)),
-
-                new Tierheimhaus(Guid.NewGuid(),
-                    "Test Tierheim 3",
-                    new Addresse("Musterstraße 3", "789", "Musterstadt 3", "Musterland 3"),
-                    TimeSpan.FromHours(10),
-                    TimeSpan.FromHours(19))
-            };
-        }
-        */
-
         
         public void OnGet()
         {
